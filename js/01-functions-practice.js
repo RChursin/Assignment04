c5b9f164d0518972dac440587a6fe972257aedef
ganstacoder <ganstacoder@gmail.com>
2024-03-12 20:33:16 -0700

    Practice with Functions

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,9 +1,29 @@
+/** Practice with Functions */
+
 //STEP 1
+function halfNumber(number) {
+    let result = number / 2;
+    console.log("Half of " + number + " is " + result + ".");
+    return result;
+}
 
 //STEP 2
+function squareNumber(number) {
+    let result = number * number;
+    console.log("The result of squaring the number " + number + " is " + result + ".");
+    return result;
+}
 
 //STEP 3
+function percentOf(num1, num2) {
+    let result = (num1 / num2) * 100;
+    console.log(num1 + " is " + result + "% of " + num2 + ".");
+    return result;
+}
 
 //STEP 4
-
-//STEP 5
+function findModulus(num1, num2) {
+    let result = num2 % num1;
+    console.log(result + " is the modulus of " + num1 + " and " + num2 + ".");
+    return result;
+}
